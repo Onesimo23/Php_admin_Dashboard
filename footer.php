225538b28899a366d5173f5d1f6077df0c9cf7aa
Onesimo23 <113595001+Onesimo23@users.noreply.github.com>
2023-03-30 21:23:53 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
             <div class="copyright">
-                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">DexignLab</a> <php</p>
+                <p>Copyright © Designed &amp; <?php echo date("Y"); ?> Developed by <a href="../index.htm" target="_blank">DexignLab</a> <php</p>
             </div>
         </div>
